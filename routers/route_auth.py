57df4371892bac4205fc9392d67c658616f5b995
sanpicule <sannsi4444@gmail.com>
2023-11-26 19:52:51 +0900

    メソッド名修正

--- routers/route_auth.py
+++ routers/route_auth.py
@@ -12,7 +12,7 @@ from fastapi_csrf_protect import CsrfProtect
 router = APIRouter()
 auth = AuthJWTCsrf()
 
-@router.post("/api/csrftoken", response_model=Csrf)
+@router.get("/api/csrftoken", response_model=Csrf)
 def get_csrf_token(csrf_protect: CsrfProtect = Depends()):
     csrf_token = csrf_protect.generate_csrf()
     res = {'csrf_token': csrf_token}
